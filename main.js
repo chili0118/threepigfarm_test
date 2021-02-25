d070d30a52d023d77a2fb9914a5c72681d9085f9
chili0118 <b10236181@gapps.uch.edu.tw>
2021-02-25 16:34:26 +0800

    RWD完成至最新消息部分

--- main.js
+++ main.js
@@ -7,4 +7,21 @@ $("#banner").slick({
 $("#farm").slick({
     slidesToShow: 3,
     slidesToScroll: 2,
+
+    responsive: [
+        {
+            breakpoint: 1025,
+            setting:{
+                slideToShow: 2,
+                slideToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 767,
+            setting:{
+                slideToShow: 1,
+                slideToScroll: 1,
+            }
+        }
+    ]
 })
